d110acc841519ce8aded243c8c2edbb6ce378dff
Prerna <chaturvediprerna03@gmail.com>
2020-02-09 18:23:10 +1300

    Updates

---
+++ precache-manifest.9e1b88a4ffbbc6e98f006a965a48da4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aece1f79366ceb397e89b929acf25ab3",
+    "url": "/crs/index.html"
+  },
+  {
+    "revision": "6e628842bf65929e1d5c",
+    "url": "/crs/static/css/main.7c4ebfa3.chunk.css"
+  },
+  {
+    "revision": "38b064e6b91a9e90bf8e",
+    "url": "/crs/static/js/2.dfcda506.chunk.js"
+  },
+  {
+    "revision": "c6967699ed8f6cf33744a1f8a8d37ba5",
+    "url": "/crs/static/js/2.dfcda506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e628842bf65929e1d5c",
+    "url": "/crs/static/js/main.efd07ec7.chunk.js"
+  },
+  {
+    "revision": "07822b6128e482793c28",
+    "url": "/crs/static/js/runtime-main.fbb30e74.js"
+  }
+]);
--- src/NoDataFoundComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NoDataFoundComponent = ({onSearchSubmit,}) =>{
-    return (
-        <span className="no-data">Sorry! No beer Found with this name, try again with different Name. </span>
-    )
-}
-
-export default NoDataFoundComponent;
